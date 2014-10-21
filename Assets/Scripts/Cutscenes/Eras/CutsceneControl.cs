68ce4e4c24681a419c3224ed3bca3ddb2b348f4a
Pedroiztpuc@gmail.com <Pedroiztpuc@gmail.com@2bea0a71-4842-6a09-00b1-f2ce8966bd63>
2014-10-21 02:51:16 +0000

    Beta 0.7: MAJOR CHANGES: - Ajustes no Inventario; - Cutscene da Historia (HistoryControl);

--- Assets/Scripts/Cutscenes/Eras/CutsceneControl.cs
+++ Assets/Scripts/Cutscenes/Eras/CutsceneControl.cs
@@ -11,23 +11,33 @@ public class CutsceneControl : MonoBehaviour {
 	public GameObject S3;
 	public GameObject S4;
 	public GameObject Text;
-	private Fade fader;
+	private HistoriaControl history;
 	bool once = false;
+	bool OnceLate = false;
 	bool once2 = false;
 	bool once3 = false;
 	bool once4 = false;
+	public bool jogoinicia = false;
 
 
 	// Use this for initialization
 	void Start () {
 
-		GameObject f = GameObject.Find ("screenFader");
-		fader = f.GetComponent<Fade> ();
+		GameObject h = GameObject.Find ("OpeningHistory");
+		history = h.GetComponent<HistoriaControl> ();
+
+
 
 		S1.SetActive (true);
+		S1.GetComponent<Animator> ().enabled = false;
 		S2.SetActive (false);
 		S3.SetActive (false);
 		S4.SetActive (false);
+
+		if (history.começacutscene == true)
+						S1.GetComponent<Animator> ().enabled = true;
+
+
 
 
 
@@ -39,30 +49,41 @@ public class CutsceneControl : MonoBehaviour {
 	void LateUpdate () {
 
 
-		Destroy (S1, ctimer);
-		if (S1 == null && once == false ) {
-						once = true;
-						S2.SetActive (true);
-			Destroy(S2, ctimer );
+
+		if (history.começacutscene == true && OnceLate == false) {
+			Start();
+			OnceLate = true;
+				}
+
+		if (history.começacutscene == true) {
+
+						Destroy (S1, ctimer);
+						if (S1 == null && once == false) {
+								once = true;
+
+								S2.SetActive (true);
+								Destroy (S2, ctimer);
 						
-				}
+						}
 
-		if (S2 == null && once2 == false) {
-			once2 = true;
-			S3.SetActive (true);
-			Destroy(S3, ctimer);
-				}
+						if (S2 == null && once2 == false) {
+								once2 = true;
+								S3.SetActive (true);
+								Destroy (S3, ctimer);
+						}
 
-		if (S3 == null && once3 == false) {
-			once3 = true;
-			S4.SetActive (true);
-			Destroy(S4, ctimer);
-		}
+						if (S3 == null && once3 == false) {
+								once3 = true;
+								S4.SetActive (true);
+								Destroy (S4, ctimer);
+						}
 
-		if (S4 == null && once4 == false) {
-			once = true;
-			Text.SetActive(false);
+						if (S4 == null && once4 == false) {
+								once = true;
+								Text.SetActive (false);
+				jogoinicia = true;
 
+						}
 				}
 
 
---
+++ Assets/Scripts/Historia/HistoriaControl.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HistoriaControl : MonoBehaviour {
+
+	public GameObject Imagem1;
+	public GameObject Imagem2;
+	public GameObject Imagem3;
+	public GameObject[] Partes = new GameObject[22];
+	public int i = 0;
+	public bool começacutscene = false;
+	bool fimdoarray = false;
+	bool disable = false;
+
+	public Color collor2;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Partes [i].renderer.enabled = true;
+		collor2 = Imagem3.renderer.material.color;
+		collor2.a = 1.0f;
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (disable == false)
+		Partes [i].renderer.enabled = true;
+
+
+		if (fimdoarray == false) {
+						if (Input.GetKeyDown (KeyCode.F)) {
+								i += 1;
+								
+								Partes [i - 1].renderer.enabled = false;
+						
+						}
+				}
+
+
+
+
+
+		if (i == 21) {
+			//Imagem3.renderer.enabled = false;
+			fimdoarray = true;
+
+			if (começacutscene == false)
+			collor2.a -= 0.1f * Time.deltaTime * 2;
+
+			if (fimdoarray == true)
+			{
+				Partes[i].renderer.enabled = false;
+				
+
+					disable = true;
+			}
+					
+
+
+
+			if (collor2.a <= 0.05f)
+			{
+				Imagem3.renderer.enabled = false;
+
+						
+						começacutscene = true;
+			}
+				}
+
+		if (i == 9) {
+						Imagem1.renderer.enabled = false;
+			Imagem2.renderer.enabled = true;
+				}
+
+		if (i == 17) {
+			Imagem2.renderer.enabled = false;
+			Imagem3.renderer.enabled = true;
+
+				}
+
+		Imagem3.renderer.material.color = collor2;
+		 
+
+
+
+
+	
+	}
+}
