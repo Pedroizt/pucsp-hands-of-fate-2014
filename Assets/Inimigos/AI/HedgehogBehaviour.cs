72e2d5158d231d8b72378ed4f03fff1bd8f8a753
Pedroiztpuc@gmail.com <Pedroiztpuc@gmail.com@2bea0a71-4842-6a09-00b1-f2ce8966bd63>
2014-10-30 01:06:44 +0000

    Versão 0.12 Conteúdo: - AI inimiga dando dano; - Arrumar a hitbox; - Arrumar a vida, colocar animação de morte e Damaged

---
+++ Assets/Inimigos/AI/HedgehogBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HedgehogBehaviour : MonoBehaviour {
+
+	public float HP;
+	public float Damage;
+	private Hpsih HPSihir;
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject h = GameObject.Find ("Senpai Notice Me");
+		HPSihir = h.GetComponent<Hpsih> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void ApplyDamage()
+	{
+		HPSihir.curHealth -= Damage;
+	}
+}
