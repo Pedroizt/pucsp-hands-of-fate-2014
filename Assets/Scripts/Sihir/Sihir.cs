915dc6cf403fce367fce371ddc32fe5168ba93ad
Pedroiztpuc@gmail.com <Pedroiztpuc@gmail.com@2bea0a71-4842-6a09-00b1-f2ce8966bd63>
2014-11-04 21:54:54 +0000

    Versão 0.18 Conteudo - SihirTests.unity - Primeira versão da sihir implementada em uma cena - Sihir.cs - Anda e vira com a camera.

---
+++ Assets/Scripts/Sihir/Sihir.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sihir : MonoBehaviour {
+
+	public Animator anim;
+	public GameObject camera;
+	Vector3 sihir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+						anim.SetBool ("Andar", true);
+						gameObject.transform.position += transform.forward;
+
+				} else
+						anim.SetBool ("Andar", false);
+
+		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, camera.transform.localEulerAngles.y, transform.localEulerAngles.z); 
+	
+	}
+}
