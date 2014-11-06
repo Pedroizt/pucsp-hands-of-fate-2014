b1ff2898fc500d17ceac5fcd5694a108987290b8
Pedroiztpuc <Pedroiztpuc@gmail.com>
2014-11-06 02:07:30 +0000

    Conteudo: Mapa e Minimapa

---
+++ Assets/Scripts/Minimapafollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimapafollow : MonoBehaviour {
+
+
+	public GameObject Target;
+	float zi;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		gameObject.transform.position = Target.transform.position;
+	
+	}
+}
