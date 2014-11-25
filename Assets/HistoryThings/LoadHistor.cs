92dab1aa414461f64277546749f920b1ac36ee29
Pedroiztpuc <Pedroiztpuc@gmail.com>
2014-11-25 23:52:41 +0000

    Menu história funciona agora.

---
+++ Assets/HistoryThings/LoadHistor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadHistor : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void OnMouseDown()
+	{
+		PlayerPrefs.SetInt( "previousLevel", Application.loadedLevel );
+		Application.LoadLevelAdditive(4);
+	}
+}
